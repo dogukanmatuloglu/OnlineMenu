310b7d8e988930e5cb8a2ddfbd1d8e0e86086da5
dogukan <dogukan.matul@gmail.com>
2022-06-05 18:54:32 +0300

    Category entity configuration yapıldı

--- OnlineMenu.Data/Configuration/CategoryConfiguration.cs
+++ OnlineMenu.Data/Configuration/CategoryConfiguration.cs
@@ -9,17 +9,14 @@ using System.Threading.Tasks;
 
 namespace OnlineMenu.Data.Configuration
 {
-    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
     {
-        public void Configure(EntityTypeBuilder<Product> builder)
+        public void Configure(EntityTypeBuilder<Category> builder)
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).UseIdentityColumn();
-            builder.Property(x => x.Price).HasPrecision(5, 2).IsRequired();
-            builder.Property(x => x.Info).HasMaxLength(200).IsRequired();
             builder.Property(x => x.Name).HasMaxLength(150).IsRequired();
-            builder.HasOne(x => x.Category).WithMany(c => c.Products).HasForeignKey(x => x.CategoryId).OnDelete(DeleteBehavior.Cascade);
-          
+            builder.HasOne(x => x.User).WithMany(x => x.Categories).HasForeignKey(x => x.UserId);
         }
     }
 }
