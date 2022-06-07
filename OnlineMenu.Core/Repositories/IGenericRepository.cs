c7949fd948eb7ef07e139fc7d0f72790614c071b
dogukan <dogukan.matul@gmail.com>
2022-06-07 03:46:30 +0300

    Repository Soyutlaması Yapıldı

---
+++ OnlineMenu.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMenu.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(int userId, Expression<Func<T, bool>> predicate = null,params Expression<Func<T, object>>[] includeProperties);
+        Task<T> GetByIdAsync(int id,int userId);
+        Task AddAysnc(T entity, int userId);
+        Task Update(T entity, int userId);
+        Task Delete(int id, int userId);
+        Task<bool> AnyAsync(Expression<Func<T,bool>> predicate, int userId);
+        Task<int> CountAsync(Expression<Func<T,bool>> predicate, int userId);
+
+
+    }
+}
---
+++ OnlineMenu.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnlineMenu.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMenu.Core.Repositories
+{
+    public interface IProductRepository:IGenericRepository<Product>
+    {
+    }
+}
