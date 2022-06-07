2a358e4b9cabd6758fecc580d6ab82b900d236d7
dogukan <dogukan.matul@gmail.com>
2022-06-07 21:52:10 +0300

    GenericRepository İmplementasyonu tamamlandı

---
+++ OnlineMenu.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using OnlineMenu.Core.Entities;
+using OnlineMenu.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMenu.Data.Repositories
+{
+    internal class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(OnlineMenuContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ OnlineMenu.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using OnlineMenu.Core.Entities;
+using OnlineMenu.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMenu.Data.Repositories
+{
+    internal class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(OnlineMenuContext context) : base(context)
+        {
+        }
+    }
+}
