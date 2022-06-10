85540b95d135e6315d30e66f7e8b0075168e4875
dogukan <dogukan.matul@gmail.com>
2022-06-10 03:47:07 +0300

    UnitOfWork implementasyonu yapıldı

---
+++ OnlineMenu.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using OnlineMenu.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMenu.Core.IUnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository productRepository { get; }
+        ICategoryRepository categoryRepository { get; }
+
+        Task SaveAsync();
+    }
+}
---
+++ OnlineMenu.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using OnlineMenu.Core.IUnitOfWork;
+using OnlineMenu.Core.Repositories;
+using OnlineMenu.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMenu.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly OnlineMenuContext _context;
+
+        public UnitOfWork(OnlineMenuContext context)
+        {
+            _context = context;
+        }
+        private ProductRepository _productRepository;
+        private CategoryRepository _categoryRepository;
+
+
+
+        public IProductRepository productRepository => _productRepository ??= new ProductRepository(_context);
+
+        public ICategoryRepository categoryRepository => _categoryRepository ??=new CategoryRepository(_context);
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
